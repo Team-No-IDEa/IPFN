224edad5a92501fd20f54f226ac87103f33a2551
James Hooper <jhooper@caci.co.uk>
2021-07-22 10:21:11 +0100

    get title

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print("Hi")
+from bs4 import BeautifulSoup
+import requests
+
+def main():
+    url = "https://www.historyofvaccines.org/content/articles/do-vaccines-cause-autism"
+    response = requests.get(url)
+
+    soup = BeautifulSoup(response.content, "html.parser")
+    print(soup.title.string)
+
+if __name__ == "__main__":
+    main()
