8853d9ae6b4ac1df6f0e842240adfa1f2440b8fa
James Hooper <jhooper@caci.co.uk>
2021-07-22 15:55:02 +0100

    pep8 stuff

--- main.py
+++ main.py
@@ -7,11 +7,9 @@ class Run():
     def __init__(self):
         print(Web.get_title(
             "https://www.historyofvaccines.org/content/articles/do-vaccines-cause-autism"))
+
         query = Web.get_title(
             "https://www.historyofvaccines.org/content/articles/do-vaccines-cause-autism")
-
-        print(Web.get_similar_sites(query))
-
         sites = Web.get_similar_sites(query)
         dates = Web.date_of_sites(sites)
         final_list = Web.combine_website_and_date(sites, dates)
